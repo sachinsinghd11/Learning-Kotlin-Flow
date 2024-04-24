3048affffd33bf494f331e83064de89bb3996674
Sachin Singh Dighan <sachinsinghd11@gmail.com>
2024-04-24 14:41:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/sachin_singh_dighan/learnkotlinproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sachin_singh_dighan.learnkotlinproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
